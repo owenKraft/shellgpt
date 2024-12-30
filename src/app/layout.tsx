ae4f123d0e03b707d1393ba1bdc514bdf795fa88
Owen Kraft <owenkraft7@gmail.com>
2024-12-30 15:16:23 -0500

    remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Terminal } from 'lucide-react'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
